515a4a94877fdeadefb71b1a212d7c6e5b2c1d13
Vagner Farias <vfarias@gmail.com>
2023-10-21 13:14:53 -0300

    creating only articles index for debugging

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -2,58 +2,11 @@
 
 namespace App\Controller;
 
-use App\Controller\AppController;
-
 class ArticlesController extends AppController
 {
-    public function initialize()
-    {
-        parent::initialize();
-
-        $this->loadComponent('Flash'); // Include the FlashComponent
-    }
-
     public function index()
     {
-        $this->set('articles', $this->Articles->find('all'));
+        $articles = $this->Articles->find('all');
+        $this->set(compact('articles'));
     }
-
-    public function view($id)
-    {
-        $article = $this->Articles->get($id);
-        $this->set(compact('article'));
-    }
-
-    public function add()
-    {
-        $article = $this->Articles->newEntity();
-        if ($this->request->is('post')) {
-            // Prior to 3.4.0 $this->request->data() was used.
-            $article = $this->Articles->patchEntity($article, $this->request->getData());
-            if ($this->Articles->save($article)) {
-                $this->Flash->success(__('Your article has been saved.'));
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('Unable to add your article.'));
-        }
-        $this->set('article', $article);
-    }
-
-    public function edit($id = null)
-    {
-    $article = $this->Articles->get($id);
-    if ($this->request->is(['post', 'put'])) {
-        // Prior to 3.4.0 $this->request->data() was used.
-        $this->Articles->patchEntity($article, $this->request->getData());
-        if ($this->Articles->save($article)) {
-            $this->Flash->success(__('Your article has been updated.'));
-            return $this->redirect(['action' => 'index']);
-        }
-        $this->Flash->error(__('Unable to update your article.'));
-    }
-
-    $this->set('article', $article);
-}
-
-
 }
